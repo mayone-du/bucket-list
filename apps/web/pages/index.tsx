4cad6cae4e6fb788aa8d4b0440b8742181bae9bd
mayone-du <cocomayo1201@icloud.com>
2022-06-15 23:10:57 +0900

    🚧 add sample code

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,15 @@
 import { NextPage } from "next";
 import { Button } from "ui";
+import { trpc } from "./_app";
 
 const IndexPage: NextPage = () => {
+  const { data, isLoading, isError } = trpc.useQuery([
+    "getUserById",
+    "hogehoge",
+  ]);
   return (
     <div>
-      top page
+      {data?.id}
       <Button />
     </div>
   );
